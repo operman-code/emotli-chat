93dc55730cb4db717aaaa6526ed4ad36fecfd1d5
operman-code <jagtapomkar3052002@gmail.com>
2025-10-19 14:38:14 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,13 +5,17 @@ import { setupChatSocket } from './src/sockets/chatSocket.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
-  cors: { origin: process.env.CLIENT_URL, methods: ['GET', 'POST'] },
+  cors: { origin: process.env.CLIENT_URL, methods: ['GET', 'POST'] }
 });
 
 setupChatSocket(io);
 
-server.listen(PORT, () => console.log(`Emotli backend running on port ${PORT}`));
+server.listen(PORT, () => {
+  console.log(`Emotli backend running on port ${PORT}`);
+});
